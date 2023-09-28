a0cf3eb6e1b2aac007c867c930b769612641633a
Rajkumar <rajkumarvb.career@gmail.com>
2023-09-28 17:58:00 +0530

    Lambda Composability - 3

--- src/LambdaComposability.java
+++ src/LambdaComposability.java
@@ -1,12 +1,14 @@
 import java.time.LocalDateTime;
 import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 import java.util.function.UnaryOperator;
 
 public class LambdaComposability{
     public static void main(String[] args) {
         UnaryOperator<Integer> increment = x -> x+1;
-        Runnable logStart = () -> System.out.println("Start : " + LocalDateTime.now());
-        Runnable logEnd = () -> System.out.println("End : " + LocalDateTime.now());
+        Consumer<String> logMessage = message -> System.out.println(message + ":"+ LocalDateTime.now());
+        Runnable logStart = () -> logMessage.accept("Start");
+        Runnable logEnd = () -> logMessage.accept("End");
         BiConsumer<UnaryOperator<Integer>, Integer> logger = (operation,input) -> {
             logStart.run();
             System.out.println(operation.apply(5));
