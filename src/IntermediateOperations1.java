6e6940154f07bc59b51e50765a37ea69e1421cd4
Rajkumar <rajkumarvb.career@gmail.com>
2023-09-29 11:06:10 +0530

    Peek Operator - Laziness

--- src/IntermediateOperations1.java
+++ src/IntermediateOperations1.java
@@ -4,10 +4,11 @@ import java.util.stream.Stream;
 public class IntermediateOperations1 {
     public static void main(String[] args) {
 
-        List<String> processedList = Stream.of("Apple", "Mango", "Banana", "Strawberry")
+        Stream<String> processedStream = Stream.of("Apple", "Mango", "Banana", "Strawberry")
                 .map(String::toUpperCase)
                 .peek(fruit -> System.out.println("Peek : " + fruit))
-                .toList();
+                .filter(fruit -> fruit.length() < 6);
+
 
     }
 }
