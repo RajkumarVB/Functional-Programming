a233c5b5bbb49ee9bdcddf67454b23622a98b095
Rajkumar <rajkumarvb.career@gmail.com>
2023-09-29 12:30:58 +0530

    Stream.concat

--- src/IntermediateOperations1.java
+++ src/IntermediateOperations1.java
@@ -12,5 +12,11 @@ public class IntermediateOperations1 {
                 .distinct()
                 .sorted()
                 .forEach(System.out::println);
+
+        Stream<Integer> oddStream = Stream.of(1, 3, 5, 7, 9);
+        Stream<Integer> evenStream = Stream.of(2,4,6,8,10);
+
+        Stream.concat(oddStream,evenStream)
+                .forEach(System.out::println);
     }
 }
