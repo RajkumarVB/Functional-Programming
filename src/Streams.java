d19a6e22e950bc4e9156d0f070fb0f9ad49d4d07
Rajkumar <rajkumarvb.career@gmail.com>
2023-09-29 10:02:37 +0530

    Streams

---
+++ src/Streams.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
+
+public class Streams {
+    public static void main(String[] args) throws IOException {
+
+        //Using stream API
+        List<String> names = List.of("Foo", "Bar","Baz");
+//        Set<String> names = Set.of("Foo", "Bar","Baz");
+        Stream<String> stream = names.stream();
+        stream.forEach(System.out::println);
+
+        //Using an Array
+        int[] numbers = {1,2,3,4,5};
+        Arrays.stream(numbers).forEach(System.out::println);
+
+        // Using stream of
+        Stream.of("Mango","Banana", "Strawberry").forEach(System.out::println);
+
+        //Using built-in APIs that return Stream
+        Stream<String> lines = Files.lines(Path.of("sample.txt"));
+        lines.forEach(System.out::println);
+
+        // Using Stream.iterate
+        Stream.iterate(0,x->x+2)
+                .limit(10)
+                .forEach(System.out::println);
+    }
+}
