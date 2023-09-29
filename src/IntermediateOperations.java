54fc6cdc9bee4f4bb011c58f3e81384e31a92870
Rajkumar <rajkumarvb.career@gmail.com>
2023-09-29 10:26:20 +0530

    Intermediate Operations - 2

--- src/IntermediateOperations.java
+++ src/IntermediateOperations.java
@@ -3,9 +3,9 @@ import java.util.stream.Stream;
 public class IntermediateOperations {
     public static void main(String[] args) {
 
-        Stream<Integer> numbers = Stream.of(1,2,3,4,5,6,7,8,9,10);
-        Stream<Integer> limitedStream = numbers.limit(5);
-        limitedStream.forEach(System.out::println);
+        Stream.of(1,2,3,4,5,6,7,8,9,10)
+                .filter(num -> num %2 ==0)
+                .forEach(System.out::println);
 
     }
 }
