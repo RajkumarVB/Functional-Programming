a93b5d8005d0d7ffc6bdacd4561633a072aba415
Rajkumar <rajkumarvb.career@gmail.com>
2023-09-29 11:04:37 +0530

    Peek Operator - 2

--- src/IntermediateOperations1.java
+++ src/IntermediateOperations1.java
@@ -1,11 +1,13 @@
+import java.util.List;
 import java.util.stream.Stream;
 
 public class IntermediateOperations1 {
     public static void main(String[] args) {
 
-        Stream.of("Apple","Mango","Banana", "Strawberry")
-                .peek(fruit -> System.out.println("Peek : "+fruit))
-                .filter(fruit -> fruit.length() <6)
-                .forEach(System.out::println);
+        List<String> processedList = Stream.of("Apple", "Mango", "Banana", "Strawberry")
+                .map(String::toUpperCase)
+                .peek(fruit -> System.out.println("Peek : " + fruit))
+                .toList();
+
     }
 }
